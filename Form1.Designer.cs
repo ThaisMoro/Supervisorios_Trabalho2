3fcb25e4b9dbd6905a7281e8fcf69a4b77f033dc
Thais Moro <thais.moro@ucl.br>
2021-11-30 10:27:03 -0300

    Modo Manual

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace Sistemas_Supervisorios___Trabalho_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {            
+            this.btn_Ext_Andar1 = new System.Windows.Forms.Button();
+            this.btn_Ext_Andar2 = new System.Windows.Forms.Button();
+            this.btn_Ext_Andar3 = new System.Windows.Forms.Button();
+            this.btn_Ext_Andar4 = new System.Windows.Forms.Button();
+            this.btn_Ext_Andar5 = new System.Windows.Forms.Button();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.lbl_EM = new System.Windows.Forms.Label();
+            this.chc_Manual = new System.Windows.Forms.CheckBox();
+            this.chc_Automatico = new System.Windows.Forms.CheckBox();
+            this.btn_Int_Andar1 = new System.Windows.Forms.Button();
+            this.btn_Int_Andar2 = new System.Windows.Forms.Button();
+            this.btn_Int_Andar3 = new System.Windows.Forms.Button();
+            this.btn_Int_Andar4 = new System.Windows.Forms.Button();
+            this.btn_Int_Andar5 = new System.Windows.Forms.Button();
+            this.lbl_Internal_Andar_Atual = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Ext_Andar1
+            // 
+            this.btn_Ext_Andar1.Location = new System.Drawing.Point(461, 243);
+            this.btn_Ext_Andar1.Name = "btn_Ext_Andar1";
+            this.btn_Ext_Andar1.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ext_Andar1.TabIndex = 2;
+            this.btn_Ext_Andar1.Text = "1º Andar";
+            this.btn_Ext_Andar1.UseVisualStyleBackColor = true;
+            this.btn_Ext_Andar1.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Ext_Andar2
+            // 
+            this.btn_Ext_Andar2.Location = new System.Drawing.Point(552, 243);
+            this.btn_Ext_Andar2.Name = "btn_Ext_Andar2";
+            this.btn_Ext_Andar2.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ext_Andar2.TabIndex = 3;
+            this.btn_Ext_Andar2.Text = "2º Andar";
+            this.btn_Ext_Andar2.UseVisualStyleBackColor = true;
+            this.btn_Ext_Andar2.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Ext_Andar3
+            // 
+            this.btn_Ext_Andar3.Location = new System.Drawing.Point(461, 214);
+            this.btn_Ext_Andar3.Name = "btn_Ext_Andar3";
+            this.btn_Ext_Andar3.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ext_Andar3.TabIndex = 0;
+            this.btn_Ext_Andar3.Text = "3º Andar";
+            this.btn_Ext_Andar3.UseVisualStyleBackColor = true;
+            this.btn_Ext_Andar3.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Ext_Andar4
+            // 
+            this.btn_Ext_Andar4.Location = new System.Drawing.Point(552, 214);
+            this.btn_Ext_Andar4.Name = "btn_Ext_Andar4";
+            this.btn_Ext_Andar4.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ext_Andar4.TabIndex = 1;
+            this.btn_Ext_Andar4.Text = "4º Andar";
+            this.btn_Ext_Andar4.UseVisualStyleBackColor = true;
+            this.btn_Ext_Andar4.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Ext_Andar5
+            // 
+            this.btn_Ext_Andar5.Location = new System.Drawing.Point(552, 185);
+            this.btn_Ext_Andar5.Name = "btn_Ext_Andar5";
+            this.btn_Ext_Andar5.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ext_Andar5.TabIndex = 4;
+            this.btn_Ext_Andar5.Text = "5º Andar";
+            this.btn_Ext_Andar5.UseVisualStyleBackColor = true;
+            this.btn_Ext_Andar5.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl_Status.Location = new System.Drawing.Point(478, 189);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Status.TabIndex = 10;
+            this.lbl_Status.Text = "Parado";
+            // 
+            // lbl_EM
+            // 
+            this.lbl_EM.AutoSize = true;
+            this.lbl_EM.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl_EM.Location = new System.Drawing.Point(127, 210);
+            this.lbl_EM.Name = "lbl_EM";
+            this.lbl_EM.Size = new System.Drawing.Size(23, 13);
+            this.lbl_EM.TabIndex = 11;
+            this.lbl_EM.Text = "EM";
+            // 
+            // chc_Manual
+            // 
+            this.chc_Manual.AutoSize = true;
+            this.chc_Manual.BackColor = System.Drawing.SystemColors.Control;
+            this.chc_Manual.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.chc_Manual.Location = new System.Drawing.Point(475, 122);
+            this.chc_Manual.Name = "chc_Manual";
+            this.chc_Manual.Size = new System.Drawing.Size(61, 17);
+            this.chc_Manual.TabIndex = 12;
+            this.chc_Manual.Text = "Manual";
+            this.chc_Manual.UseVisualStyleBackColor = false;
+            // 
+            // chc_Automatico
+            // 
+            this.chc_Automatico.AutoSize = true;
+            this.chc_Automatico.BackColor = System.Drawing.SystemColors.Control;
+            this.chc_Automatico.Location = new System.Drawing.Point(552, 122);
+            this.chc_Automatico.Name = "chc_Automatico";
+            this.chc_Automatico.Size = new System.Drawing.Size(79, 17);
+            this.chc_Automatico.TabIndex = 13;
+            this.chc_Automatico.Text = "Automático";
+            this.chc_Automatico.UseVisualStyleBackColor = false;
+            // 
+            // btn_Int_Andar1
+            // 
+            this.btn_Int_Andar1.Location = new System.Drawing.Point(170, 268);
+            this.btn_Int_Andar1.Name = "btn_Int_Andar1";
+            this.btn_Int_Andar1.Size = new System.Drawing.Size(75, 23);
+            this.btn_Int_Andar1.TabIndex = 14;
+            this.btn_Int_Andar1.Text = "1º Andar";
+            this.btn_Int_Andar1.UseVisualStyleBackColor = true;
+            this.btn_Int_Andar1.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Int_Andar2
+            // 
+            this.btn_Int_Andar2.Location = new System.Drawing.Point(170, 233);
+            this.btn_Int_Andar2.Name = "btn_Int_Andar2";
+            this.btn_Int_Andar2.Size = new System.Drawing.Size(75, 23);
+            this.btn_Int_Andar2.TabIndex = 15;
+            this.btn_Int_Andar2.Text = "2º Andar";
+            this.btn_Int_Andar2.UseVisualStyleBackColor = true;
+            this.btn_Int_Andar2.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Int_Andar3
+            // 
+            this.btn_Int_Andar3.Location = new System.Drawing.Point(170, 198);
+            this.btn_Int_Andar3.Name = "btn_Int_Andar3";
+            this.btn_Int_Andar3.Size = new System.Drawing.Size(75, 23);
+            this.btn_Int_Andar3.TabIndex = 16;
+            this.btn_Int_Andar3.Text = "3º Andar";
+            this.btn_Int_Andar3.UseVisualStyleBackColor = true;
+            this.btn_Int_Andar3.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Int_Andar4
+            // 
+            this.btn_Int_Andar4.Location = new System.Drawing.Point(170, 163);
+            this.btn_Int_Andar4.Name = "btn_Int_Andar4";
+            this.btn_Int_Andar4.Size = new System.Drawing.Size(75, 23);
+            this.btn_Int_Andar4.TabIndex = 17;
+            this.btn_Int_Andar4.Text = "4º Andar";
+            this.btn_Int_Andar4.UseVisualStyleBackColor = true;
+            this.btn_Int_Andar4.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // btn_Int_Andar5
+            // 
+            this.btn_Int_Andar5.Location = new System.Drawing.Point(170, 128);
+            this.btn_Int_Andar5.Name = "btn_Int_Andar5";
+            this.btn_Int_Andar5.Size = new System.Drawing.Size(75, 23);
+            this.btn_Int_Andar5.TabIndex = 18;
+            this.btn_Int_Andar5.Text = "5º Andar";
+            this.btn_Int_Andar5.UseVisualStyleBackColor = true;
+            this.btn_Int_Andar5.Click += new System.EventHandler(this.ControlPanel);
+            // 
+            // lbl_Internal_Andar_Atual
+            // 
+            this.lbl_Internal_Andar_Atual.AutoSize = true;
+            this.lbl_Internal_Andar_Atual.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl_Internal_Andar_Atual.Location = new System.Drawing.Point(189, 112);
+            this.lbl_Internal_Andar_Atual.Name = "lbl_Internal_Andar_Atual";
+            this.lbl_Internal_Andar_Atual.Size = new System.Drawing.Size(16, 13);
+            this.lbl_Internal_Andar_Atual.TabIndex = 19;
+            this.lbl_Internal_Andar_Atual.Text = "---";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Internal_Andar_Atual);
+            this.Controls.Add(this.btn_Int_Andar5);
+            this.Controls.Add(this.btn_Int_Andar4);
+            this.Controls.Add(this.btn_Int_Andar3);
+            this.Controls.Add(this.btn_Int_Andar2);
+            this.Controls.Add(this.btn_Int_Andar1);
+            this.Controls.Add(this.chc_Automatico);
+            this.Controls.Add(this.chc_Manual);
+            this.Controls.Add(this.lbl_EM);
+            this.Controls.Add(this.lbl_Status);
+            this.Controls.Add(this.btn_Ext_Andar5);
+            this.Controls.Add(this.btn_Ext_Andar2);
+            this.Controls.Add(this.btn_Ext_Andar1);
+            this.Controls.Add(this.btn_Ext_Andar4);
+            this.Controls.Add(this.btn_Ext_Andar3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Ext_Andar3;
+        private System.Windows.Forms.Button btn_Ext_Andar4;
+        private System.Windows.Forms.Button btn_Ext_Andar1;
+        private System.Windows.Forms.Button btn_Ext_Andar2;
+        private System.Windows.Forms.Button btn_Ext_Andar5;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.Label lbl_EM;
+        private System.Windows.Forms.CheckBox chc_Manual;
+        private System.Windows.Forms.CheckBox chc_Automatico;
+        private System.Windows.Forms.Button btn_Int_Andar1;
+        private System.Windows.Forms.Button btn_Int_Andar2;
+        private System.Windows.Forms.Button btn_Int_Andar3;
+        private System.Windows.Forms.Button btn_Int_Andar4;
+        private System.Windows.Forms.Button btn_Int_Andar5;
+        private System.Windows.Forms.Label lbl_Internal_Andar_Atual;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistemas_Supervisorios___Trabalho_2
+{
+    public partial class Form1 : Form
+    {
+        int andarAtual = 1;
+        static List<String> filaAndaresChamados = new List<String>();     
+        static CancellationTokenSource src = new CancellationTokenSource();
+        CancellationToken ct = src.Token;
+
+        public Form1()
+        {                
+            InitializeComponent();
+            ResetInternalNavegationPanel();
+            Task t = Task.Run(() => MoveElevator());    
+        }
+
+        private void ControlPanel(object sender, EventArgs e)
+        {
+            var andar = (sender as Button).Text;
+
+            var local = ((sender as Button).Name).Contains("Int")?"Internamente":"Externamente";
+            
+            switch (andar)
+            {
+                case "1º Andar":
+                    filaAndaresChamados.Add(local + ";1");
+                    break;
+                case "2º Andar":
+                    filaAndaresChamados.Add(local + ";2");
+                    break;
+                case "3º Andar":
+                    filaAndaresChamados.Add(local + ";3");
+                    break;
+                case "4º Andar":
+                    filaAndaresChamados.Add(local + ";4");
+                    break;
+                case "5º Andar":
+                    filaAndaresChamados.Add(local + ";5");
+                    break;
+            }
+        }
+
+        public async Task MoveElevator()
+        {
+            while (true)
+            {
+                while (filaAndaresChamados.Count() > 0)
+                {
+                    var local = filaAndaresChamados.First().Split(';')[0];
+                    var nAndar = int.Parse(filaAndaresChamados.First().Split(';')[1]);
+
+                    if (nAndar > andarAtual)
+                    {                        
+                        Invoke(new Action(() => { lbl_EM.BackColor = Color.Red; }));
+                        Invoke(new Action(() => { lbl_Status.Text = "Subindo"; }));
+
+                        while (nAndar > andarAtual)
+                        {
+                            await Task.Delay(2000);
+                            andarAtual++;
+                            Invoke(new Action(() => { lbl_Internal_Andar_Atual.Text = andarAtual.ToString(); }));                            
+
+                        }
+                    }
+                    else if (nAndar < andarAtual)
+                    {
+                        Invoke(new Action(() => { lbl_EM.BackColor = Color.Red; }));
+                        Invoke(new Action(() => { lbl_Status.Text = "Descendo"; }));
+
+                        while (nAndar < andarAtual)
+                        {
+                            await Task.Delay(2000);
+                            andarAtual--;
+                            Invoke(new Action(() => { lbl_Internal_Andar_Atual.Text = andarAtual.ToString(); }));
+                        }
+                    }
+
+                    filaAndaresChamados.RemoveAt(0);                 
+                    Invoke(new Action(() => { lbl_EM.BackColor = Color.White; }));
+                    Invoke(new Action(() => { lbl_Status.Text = "Parado"; }));
+                    await OpenAndCloseElavatorPort();
+
+                }
+            }
+        }
+
+        public async Task OpenAndCloseElavatorPort()
+        {
+            Invoke(new Action(() => { lbl_Internal_Andar_Atual.BackColor = Color.Green; }));
+            await Task.Delay(1000);
+            Invoke(new Action(() => { lbl_Internal_Andar_Atual.BackColor = Color.White; }));
+            await Task.Delay(1000);
+            Invoke(new Action(() => { lbl_Internal_Andar_Atual.BackColor = Color.Green; }));
+            await Task.Delay(1000);
+            Invoke(new Action(() => { lbl_Internal_Andar_Atual.BackColor = Color.White; }));
+            await Task.Delay(1000);
+        }
+ 
+
+        public void ResetInternalNavegationPanel()
+        {            
+            lbl_EM.BackColor = Color.White;
+            lbl_Status.Text = "Parado";
+            chc_Manual.Checked = true;
+            chc_Automatico.Checked = false;            
+            andarAtual = 1;
+            lbl_Internal_Andar_Atual.Text = andarAtual.ToString();
+        }
+
+        public void ControlExternalNavegationPanel(bool enable)
+        {
+            Button[] botoes = { btn_Ext_Andar1, btn_Ext_Andar2, btn_Ext_Andar3, btn_Ext_Andar4, btn_Ext_Andar5 };
+            foreach(var botao in botoes)
+            {
+                botao.Enabled = enable;
+            }
+        }
+
+
+
+
+    }
+}
